ae8053d3cae8b10c6046a16101f65f49605044a0
daithi-coombes <webeire@gmail.com>
2013-11-04 16:49:16 +0000

    - update gitignore  - add sample logstash config  - clean lumberjack config  - remove 'winston' dependency. Needs custom logger so use nodejs 'fs' to handle logs  - get data on all flows

--- server.js
+++ server.js
@@ -22,8 +22,10 @@
 
 //vars
 var flow = require('./flowdockConfig');
+var fs = require('fs');
 var target = '/flows/' + flow.organization + '/' + flow.flow + '/users';
 console.log(target);
+var count = 0;
 var http = require('http');
 var https = require('https');
 var endpoint = 'https://' + flow.token + '@api.flowdock.com';
@@ -62,20 +64,55 @@ var FlowDock = {
 
 	},
 
-	requestGet : function(){
+	requestGet : function( flowName ){
 
 		console.log('Request sent');
 
 		https.get(
-			endpoint + target,
+			FlowDock.url,
 			function( resp ){
 				resp.setEncoding('utf8');
-				resp.on( 'data', FlowDock.parseResponse );
+				resp.on( 'data', function(res){
+					FlowDock.parseResponse(flowName, res);
+				});
 			}
 		).on('error',function(res){
 			console.log('Got error ' + res.message );
 		});
 
+	},
+
+	getFlows : function(){
+
+		var exec = require('child_process').exec;
+
+		exec('wc -l flowdock.log | cut -f1 -d\' \'', function (error, results) {
+		    lineNum = results.trim();
+		    if( lineNum>1000 ){
+		    	console.log('Backing up flowdock.log file');
+		    	exec('mv flowdock.log flowdock.log.bak');
+		    }
+		});
+
+		console.log('Requesting flows');
+
+		https.get(
+			endpoint + '/flows',
+			function( resp ){
+				resp.setEncoding('utf8');
+				resp.on( 'data', function(res){
+					FlowDock.flows, j = JSON.parse(res);
+
+					for(var x=0; x<j.length; x++){
+						var flowName = j[x].parameterized_name;
+						target = '/flows/' + flow.organization + '/' + flowName + '/users';
+						FlowDock.url = endpoint + target;
+						console.log( 'target =========>' + target );
+						FlowDock.requestGet( flowName );
+					}
+				});
+			}
+		);
 	},
 
 	/**
@@ -84,24 +121,40 @@ var FlowDock = {
 	 * @param  {http.ServerResponse} resp The server response
 	 * @return {void}
 	 */
-	parseResponse :  function (chunk) {
+	parseResponse :  function (flowName, chunk) {
 		console.log('Parsing response...');
+		count++;
+		console.log( count + ' events sent since start');
+
 		var j = JSON.parse(chunk);
 		var res = [];
-		for(var x=0; x<j.length; x++)
-			res.push({
+		for(var x=0; x<j.length; x++){
+			res = {
 				id : j[x].id,
+				flow : flowName,
+				organization : flow.organization,
 				nick : j[x].nick,
 				last_activity : new Date(j[x].last_activity)
-			});
-		logger.warn( JSON.stringify(res) );
+			};
+			FlowDock.log( res );
+		}
 		//FlowDock.serverResponse.end( JSON.stringify(res) );
+	},
+
+	/**
+	 * Log data to a file
+	 * @param  {[type]} data [description]
+	 * @return {[type]}      [description]
+	 */
+	log : function( data ){
+		var log = fs.createWriteStream('flowdock.log', {'flags': 'a'});
+		log.write( JSON.stringify( data ) + '\n' );		
 	}
 };
 
 //call FlowDock.requestGet every 5 minutes
-FlowDock.requestGet();
-setInterval(function(){ FlowDock.requestGet(); }, 300000);
+FlowDock.getFlows();
+setInterval(function(){ FlowDock.getFlows(); }, 1000);
 
 //server
 //http.createServer(FlowDock.serverStart).listen(1337, '127.0.0.1');
