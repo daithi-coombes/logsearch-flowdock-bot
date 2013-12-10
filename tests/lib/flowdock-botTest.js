b411a38d2d1479552b3422a83d220774408393c7
daithi-coombes <webeire@gmail.com>
2013-12-10 11:06:26 +0000

    test cases concept

--- tests/lib/flowdock-botTest.js
+++ tests/lib/flowdock-botTest.js
@@ -1,6 +1,39 @@
-var assert = require('assert'),
-	flowdock = require('../../lib/flowdock-bot');
+var assert = require('assert');
 
 suite('flowdock-bot', function(){
+	var _flowdock;
+	var _console;
+
+	setup(function(){
+		_flowdock = require('../../lib/flowdock-bot');
+	});
+	teardown(function(){
+		_flowdock = undefined;
+	});
+
+
+	test('flowdock-bot.error()', function(){
+	});
+
+	test('flowdock-bot.requestGet()', function(){
+
+	});
+
+	test('flowdock-bot.getFlows()', function(){
+
+	});
+
+	test('flowdock-bot.parseResponse()', function(){
+
+	});
+
+	test('flowdock-bot.log()', function(){
+
+	});
+
+	test('flowdock-bot.logLineCount()', function(){
+
+	});
+
 	test('returns a true boolean value', function(){return true;});
 });
