156104ac389058ca8f208e254f50377220d1bdb3
daithi-coombes <webeire@gmail.com>
2013-12-14 17:20:24 +0000

    unit test catch callback errors to errorTest()

--- tests/lib/flowdock-botTest.js
+++ tests/lib/flowdock-botTest.js
@@ -79,35 +79,35 @@ suite('flowdock-bot', function(){
 
 	test('parse user data for flow: FlowDock.parseResponse', function(done){
 	
-		fs.unlinkSync( _flowdock.filename );
+		fs.unlink( _flowdock.filename, function(){
+			var expected = [{
+						id : 1,
+						flow: 'test',
+						organization: _flowdock.config.organization,
+						nick: 'coombesy',
+						last_activity: '2013-05-08T16:07:23.180Z'
+					}];
+			_flowdock.parseResponse('test', JSON.stringify(expected), function(){});
 
-		var expected = [{
-					id : 1,
-					flow: 'test',
-					organization: _flowdock.config.organization,
-					nick: 'coombesy',
-					last_activity: '2013-05-08T16:07:23.180Z'
-				}];
-		_flowdock.parseResponse('test', JSON.stringify(expected), function(){});
-
-		fs.readFile(_flowdock.filename, 'utf8', function(err, data){
-			if (err)
-				throw new Error(err);
-			
-			actual = JSON.parse(data.trim());
-			assert.deepEqual(
-				actual,
-				expected[0]
-			);
-			done();
-		});
-});
+			fs.readFile(_flowdock.filename, 'utf8', function(err, data){
+				if (err)
+					errorTest(err);
+				
+				actual = JSON.parse(data.trim());
+				assert.deepEqual(
+					actual,
+					expected[0]
+				);
+				done();
+			});
+		} );
+	});
 
 	test('Test log file is being written to: FlowDock.log()', function(done){
 
 		fs.readFile(_flowdock.filename, 'utf8', function(err, data){
 			if (err)
-				throw new Error(err)
+				errorTest(err);
 
 			data = JSON.parse(data.trim());
 			assert.deepEqual(data, _logEvent);
@@ -145,5 +145,5 @@ suite('flowdock-bot', function(){
 });
 
 function errorTest(e, data){
-	throw new Error(data);
+	throw new Error(e);
 }
